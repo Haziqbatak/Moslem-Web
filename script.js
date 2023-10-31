891486896b7deead6a36a861f465d27abae11d49
Haziqbatak <haziqimtiyaz17@gmail.com>
2023-10-31 07:56:30 +0700

    new change

--- script.js
+++ script.js
@@ -80,27 +80,15 @@ fetch(jadwalAPI)
     })
 }) 
 
-function showTime() {
-    var date = new Date();
-    var h = date.getHours();//0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    if (h == 0) {
-        h = 12;
-    }
-    if (h > 12) {
-        h = h - 12;
-        session = "PM";
-    }
-    h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + m : m;
-    s = s < 10 ? "0" + s : s;
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    setTimeout(showTime, 1000);
-}
-showTime();
+let hrs = document.getElementById("hrs");
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
 
+setInterval(()=>{
+    let currentTime = new Date();
+
+    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+},1000)
 
